06ddcf226f83fd0c1066883509150ac5a5ecff6c
DingZeAn <andy856996@gmail.com>
2022-03-14 01:50:23 +0800

    I don't say any more

--- olipriceAnalyze/dataAnalyzeUsingOilPrice.R
+++ olipriceAnalyze/dataAnalyzeUsingOilPrice.R
@@ -3,3 +3,5 @@ theUrl <- "crude-oil-price.csv"
 oilprice <- fread(input=theUrl, sep=',', header=TRUE)
 #----------------------------#
 head(oilprice)
+#不適合繪製直方圖
+#hist(oilprice$price,main="oil price",xlab = "price",ylab = "num")
