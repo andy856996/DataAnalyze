d7f4bef0d75ab4a0f281502e964ea2edb4f4d949
DingZeAn <andy856996@gmail.com>
2022-03-26 21:30:04 +0800

    I don't want to say anything

--- mediumTest/iris.R
+++ mediumTest/iris.R
@@ -1,21 +1,11 @@
+rm(list=ls())
 library(factoextra)
 head(iris)
 data <- iris[, -5] # 因為Species是第五欄位，故移除掉
 head(data)         # 現在data只剩下前四個欄位的資料
-E.dist <- dist(data, method="euclidean") # 歐式距離
-M.dist <- dist(data, method="manhattan") # 曼哈頓距離
-par(mfrow=c(1,2)) # 讓圖片以1x2的方式呈現，詳情請見(4)繪圖-資料視覺化
-
-# 使用歐式距離進行分群
-h.E.cluster <- hclust(E.dist)
-plot(h.E.cluster, xlab="歐式距離")
-
-# 使用曼哈頓距離進行分群
-h.M.cluster <- hclust(M.dist) 
-plot(h.M.cluster, xlab="曼哈頓距離")
 
 # 分成三群
-kmeans.cluster <- kmeans(data, centers=3) 
+kmeans.cluster <- kmeans(data, centers=2) 
 
 # 群內的變異數
 kmeans.cluster$withinss
---
+++ mediumTest/tomato.R
@@ -0,0 +1,1 @@
+#https://www.jaredlander.com/datasets/
