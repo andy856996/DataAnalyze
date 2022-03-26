393a028577f1cb9fcf912f3680a2f3a02b26d7b2
DingZeAn <andy856996@gmail.com>
2022-03-26 21:57:36 +0800

    I don't want to say anything

--- mediumTest/tomato.R
+++ mediumTest/tomato.R
@@ -1,1 +1,3 @@
-#https://www.jaredlander.com/datasets/
+rm(list=ls())
+#https://www.jaredlander.com/data/tomatofirst.csv
+data_tomato <- read.csv("https://www.jaredlander.com/data/tomatofirst.csv", header=TRUE, sep=",", fileEncoding = 'BIG5')
--- mediumTest/wine.R
+++ mediumTest/wine.R
@@ -1,1 +1,4 @@
-#https://www.jaredlander.com/datasets/
+rm(list=ls())
+#https://www.jaredlander.com/data/wine.csv
+data_wine <- read.csv("https://www.jaredlander.com/data/wine.csv", header=TRUE, sep=",", fileEncoding = 'BIG5')
+head(data_tomato)
