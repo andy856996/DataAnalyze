370da11d1873753a9a843cbabf5bfe2a654922a8
DingZeAn <andy856996@gmail.com>
2022-03-30 14:30:39 +0800

    I don't want to say anytihng

--- mediumTest/iris.R
+++ mediumTest/iris.R
@@ -6,8 +6,10 @@ head(iris)
 data <- iris[, -5] # 因為Species是第五欄位，故移除掉
 head(data)         # 現在data只剩下前四個欄位的資料
 iris_1 = iris
+summary(iris_1$Species)
+
 # 分成三群
-kmeans.cluster <- kmeans(data, centers=10) 
+kmeans.cluster <- kmeans(data, centers=2) 
 
 # 群內的變異數
 kmeans.cluster$withinss
@@ -19,3 +21,4 @@ fviz_cluster(kmeans.cluster,           # 分群結果
              data = data,              # 資料
              geom = c("point","text"), # 點和標籤(point & label)
              frame.type = "norm")      # 框架型態
+
