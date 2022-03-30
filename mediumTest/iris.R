8d6708a86de7c903928abb3ab09fc2446bffae3c
DingZeAn <andy856996@gmail.com>
2022-03-30 13:52:09 +0800

    first commit

--- mediumTest/iris.R
+++ mediumTest/iris.R
@@ -1,11 +1,13 @@
 rm(list=ls())
+library(data.table) ##載入
 library(factoextra)
+library(dplyr) ##載入
 head(iris)
 data <- iris[, -5] # 因為Species是第五欄位，故移除掉
 head(data)         # 現在data只剩下前四個欄位的資料
-
+iris_1 = iris
 # 分成三群
-kmeans.cluster <- kmeans(data, centers=2) 
+kmeans.cluster <- kmeans(data, centers=10) 
 
 # 群內的變異數
 kmeans.cluster$withinss
