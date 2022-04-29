c475c463ede9713af271f45d15eff1c8b6f88c3e
DingZeAn <andy856996@gmail.com>
2022-04-30 04:30:03 +0800

    commit a new neural network

--- mediumTest/tomato.R
+++ mediumTest/tomato.R
@@ -3,8 +3,9 @@ library(ggplot2)
 #圓番茄 價格 來源 甜 酸 顏色 質地 總體 平均值 總計 平均值
 #https://www.jaredlander.com/data/tomatofirst.csv
 data_tomato <- read.csv("https://www.jaredlander.com/data/TomatoFirst.csv", header=TRUE, sep=",", fileEncoding = 'BIG5')
+data_tomato[is.na(data_tomato$Price),"Price"] = mean(data_tomato$Price,na.rm = 1) #用平均數補充income的缺失值
 head(data_tomato)
-
+## 檢定常態性##
 #shapiro.test(data_tomato$Round)標籤檢定無意義
 #shapiro.test(data_tomato$Tomato)非數值不能檢定
 shapiro.test(data_tomato$Price)
@@ -14,10 +15,12 @@ shapiro.test(data_tomato$Acid)
 shapiro.test(data_tomato$Color)
 shapiro.test(data_tomato$Texture)
 
-data(tips , package = "reshape2")
-head(tips)
+#var.test(data_tomato$Sweet, data_tomato$Texture)
+#ansari.test(data_tomato$Sweet, data_tomato$Texture)
+## 分析相關性  ##
+cor(data_tomato$Sweet, data_tomato$Texture)
+cor(data_tomato$Price, data_tomato$Texture)
+cor(data_tomato$Sweet, data_tomato$Color)
+cor(data_tomato$Sweet, data_tomato$Acid)
 
 
-
-T_tomato <- t.test(data_tomato$Price, alternative = "two.sided", mu = 2.5)
-data_tomato[!is.na(data_tomato)]
--- mediumTest/wine.R
+++ mediumTest/wine.R
@@ -1,20 +1,68 @@
 rm(list=ls())
+library(neuralnet)
 #品種 酒精 蘋果酸 灰分 灰分的鹼度 鎂 總酚 黃酮 非黃酮酚 原花青素 顏色強度 色相 稀釋葡萄酒的 OD280/OD315 脯氨酸  
 #https://www.jaredlander.com/data/wine.csv
 data_wine <- read.csv("https://www.jaredlander.com/data/wine.csv", header=TRUE, sep=",", fileEncoding = 'BIG5')
 head(data_wine)
-data_wine_Wo_Cultivar	 <- data_wine[, -1]
 
-# 分成三群
-kmeans.cluster <- kmeans(data_wine_Wo_Cultivar, centers=2) 
 
-# 群內的變異數
-kmeans.cluster$withinss
-# 分群結果和實際結果比較
-table(kmeans.cluster$cluster, data_wine$Cultivar)  
-# 視覺化 k-means 分群結果(基於ggplot2的語法)
-require(factoextra)
-fviz_cluster(kmeans.cluster,           # 分群結果
-             data = data_wine,              # 資料
-             geom = c("point","text"), # 點和標籤(point & label)
-             ellipse.type = "norm")      # 框架型態
+#分類資料 分成TEST 和 train 
+first_data = data_wine[1:59,]
+second_data = data_wine[60:130,]
+third_data = data_wine[131:178,]
+#(七成訓練三成預測)
+f_size = round(0.7 * nrow(first_data))#first
+s_size = round(0.7 * nrow(second_data))#second
+t_size = round(0.7 * nrow(third_data)) #third
+
+train_data <- rbind(first_data[1:f_size,], second_data[1:s_size,],third_data[1:t_size,])
+test_data  <- rbind(first_data[(f_size+1):nrow(first_data),], second_data[(s_size+1):nrow(second_data),],third_data[(t_size+1):nrow(third_data),])
+#整理資料
+train_data$first <- ifelse(train_data$Cultivar == 1, 1, 0)
+train_data$second <- ifelse(train_data$Cultivar == 2, 1, 0)
+train_data$third <- ifelse(train_data$Cultivar == 3, 1, 0)
+
+test_data$first <- ifelse(test_data$Cultivar == 1, 1, 0)
+test_data$second <- ifelse(test_data$Cultivar == 2, 1, 0)
+test_data$third <- ifelse(test_data$Cultivar == 3, 1, 0)
+
+#訓練模型
+f1 <- as.formula('first + second + third  ~ Alcohol + Malic.acid + Ash + Alcalinity.of.ash + Magnesium + Total.phenols + Flavanoids + Nonflavanoid.phenols + Proanthocyanins + Color.intensity + Hue + OD280.OD315.of.diluted.wines + Proline')
+bpn <- neuralnet(formula = f1, data = train_data, hidden = c(5,5),learningrate = 0.01)
+predicted_data <- compute(bpn,test_data)
+predictions <- predicted_data$net.result
+
+
+AnsOutPut <-  rep(0,length(1:nrow(predictions)))
+ans = 0
+for(i in c(1:nrow(predictions))){ # for-loop裡，i會依序帶入1~135的值，重複進行括號內的程式碼
+  if(predictions[i,1] > predictions[i,2]){
+    ans = 1
+  }else{
+    ans = 2
+  }
+  if(predictions[i,2] > predictions[i,3]){
+    ans = 2
+  }else{
+    ans = 3
+  }
+  if(predictions[i,1] > predictions[i,3]){
+    ans = 1
+  }else{
+    ans = 3
+  }
+  AnsOutPut[i] = ans
+}
+
+
+
+
+error <- data.frame(test_data$Cultivar,AnsOutPut)
+library(ggplot2)
+ggplot(error,aes(x=test_data$Cultivar,y=AnsOutPut)) + geom_point() + stat_smooth()
+
+
+print(bpn)
+
+#圖解BP
+plot(bpn)
